9c538fa56caeaab5792be96c456e036658c38e1b
magn3054 <mdch20@student.aau.dk>
2024-11-19 00:01:47 +0100

    maps sat op, marker skal redigeres

--- js/map.js
+++ js/map.js
@@ -1,19 +1,29 @@
 "use strict";
 
 function initMap() {
-    // Map options
     const options = {
-        zoom: 1,
-        center: { lat: 37.7749, lng: -122.4194 }
+        zoom: 18,
+        center: { lat: 56.1513500, lng: 10.1847500 },
+        center: { lat: 56.1513500, lng: 10.1850000 },
+        mapTypeId: "satellite",
+        mapId: "7a003606d996bab5",
+        // disableDefaultUI: false, // Keep the default UI but hide specific controls
+        zoomControl: true, // Hides the zoom control
+        streetViewControl: false, // Hides the Street View control
+        mapTypeControl: false, // Hides the map type switcher
+        fullscreenControl: true
     };
 
-    // Initialize map
     const map = new google.maps.Map(document.getElementById("smovs-maps-location"), options);
+    const markerElement = document.createElement("div");
+    markerElement.id = "map-marker";
 
-    // Add a marker
-    const marker = new google.maps.Marker({
-        position: { lat: 37.7749, lng: -122.4194 },
+    map.setHeading(90);
+
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+        position: { lat: 56.1513500, lng: 10.1847500 },
         map: map,
-        title: "Your Business"
+        content: markerElement,
+        title: "SMOVS"
     });
 }
